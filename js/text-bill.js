3b71bcb648f31ae9091099e9e99158d9b15864b9
MJ-12-builder2 <m_ngwekazi@yahoo.com>
2021-08-13 22:10:47 +0200

    initial commit

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,8 +1,38 @@
 // get a reference to the textbox where the bill type is to be entered
+    // these variables are global and defined outside of the Add button event listener.
+    var callsTotal = 0;
+    var smsTotal = 0;
+
+// the event listener is declared below here
+    //get the value entered into the bill type text field,
+    //check if it is a 'call' or 'sms',
+    //add the correct value to the callsTotal or smsTotal variable,
+    //and update the totals that is displayed on the screen.
+
 
 //get a reference to the add button
+    textTotalAddBtn.addEventListener('click', textBillTotal);
 
 //create a variable that will keep track of the total bill
+function textBillTotal(){
+    // get the value entered in the billType textfield
+    var billTypeEntered = billTypeText.value.trim();
+    // update the correct total
+    if (billTypeEntered === "bill"){
+        callsTotal += 2.75
+    }
+    else if (billTypeEntered === "sms"){
+        smsTotal += 0.75;
+    }
+    
+    //update the totals that is displayed on the screen.
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+    }
+
+    textTotalAddBtn.addEventListener('click', textBillTotal);
 
 //add an event listener for when the add button is pressed
 
@@ -10,3 +40,22 @@
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen
+    function textBillTotal(){
+
+    // ... other code here
+    
+    //update the totals that is displayed on the screen.
+    callsTotalElem.innerHTML = callsTotal.toFixed(2);
+    smsTotalElem.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalCostElem.innerHTML = totalCost.toFixed(2);
+    
+    //color the total based on the criteria
+    if (totalCost >= 50){
+        // adding the danger class will make the text red
+        totalCostElem.classList.add("danger");
+    }
+    else if (totalCost >= 30){
+        totalCostElem.classList.add("warning");
+    }
+    }
