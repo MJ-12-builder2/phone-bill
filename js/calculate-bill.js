7a4025d5f8e16190a7dbfcc7196f0ded0f66dc47
MJ-12-builder2 <m_ngwekazi@yahoo.com>
2021-08-16 13:27:14 +0200

    calculate-bill

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,35 +1,11 @@
 //get a reference to the calculate button
-    // add event listener
-    function calculateBtnClicked(){
-    // logic goes here
-    }
+    const calculateBtnElement = document.querySelector(".calculateBtn");
 
 //get a reference to the billTotal element
-    function calculateBtnClicked(){
-    // get the string entered in the textArea
-    var billString = billStringElement.value;
-    //split the string
-    var billItems = billString.split(",");
-    // a variable for the total phone bill.
-    var billTotal = 0;
-    //loop over all the bill items
-    for (var i=0;i<billItems.length;i++){
-        var billItem = billItems[i].trim();
-        if (billItem === "call"){
-            billTotal += 2.75;
-        }
-        else if (billItem === "sms"){
-            billTotal += 0.75;
-        }
-    }
-    
-    //round to two decimals
-    var roundedBillTotal = billTotal.toFixed(2);
-    billTotalElement.innerHTML = roundedBillTotal;
-    }
+   const billTotalElement = document.querySelector(".billTotal");
 
 //get a reference to the billString
-const billStringElement = document.querySelector(".billString");
+    const billStringElement = document.querySelector(".billString");
 // do this for all the DOM elements
    
 //create the function that will be called when the calculate button is pressed
@@ -40,20 +16,35 @@ const billStringElement = document.querySelector(".billString");
     } 
 
 //  * this function should read the string value entered - split it on a comma.
-    function calculateBtnClicked(){
-    // logic goes here
-        calculateBtnClicked.addEventListener('click', calculateBtnClicked);
-    } 
-
+        function calculateBtnClicked(){
+        // get the string entered in the textArea
+        var billString = billStringElement.value;
+        //split the string
+        }
 
 //  * loop over all the entries in the the resulting list
-
-
+        var billString = billStringElement.value;
+        var billItems = billString.split(",");
+      
+        for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+        billTotal += 2.75;
+    }
+}
 
 //  * check if it is a call or an sms and add the right amount to the overall total
-
+    if (billItem === "call"){
+        billTotal += 2.75;
+    }
+    else if (billItem === "sms"){
+        billTotal += 0.75;
+    }
 
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
+    var billTotal = 0;
+    var roundedBillTotal = billTotal.toFixed(2);
+        billTotalElement.innerHTML = roundedBillTotal;
 
 
 //link the function to a click event on the calculate button
